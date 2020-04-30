08bd99f188a4f69a31876253e2e12f70c83ac092
Bruce P <treble37@users.noreply.github.com>
2020-04-30 16:36:23 -0700

    add todos, port over processExplain wip

--- README.md
+++ README.md
@@ -13,3 +13,7 @@
 cargo run -- '[{"Plan":{"Alias":"c0","Node Type":"Seq Scan","Parallel Aware":false,"Plan Rows":50,"Plan Width":1572,"Relation Name":"coaches","Startup Cost":0.0,"Total Cost":10.5}}]'
 
 cargo run -- '[{"Plan":{"Alias":"c0"}}]'
+
+### Todos
+
+- [ ] Add back Triggers struct in Explain
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,33 @@ use std::fmt;
 
 const UNDER: &str = "Under";
 const OVER: &str = "Over";
+const CTE_SCAN: &str = "CTE Scan";
 
 type NodeType = String;
 type EstimateDirection = String;
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all="snake_case")]
+struct Explain {
+    //TODO: add Triggers back, add default for plan?
+    plan: Plan,
+    #[serde(default)]
+    planning_time: f64,
+    #[serde(default)]
+    execution_time: f64,
+    #[serde(default)]
+    total_cost: f64,
+    #[serde(default)]
+    max_rows: u64,
+    #[serde(default)]
+    max_cost: f64,
+    #[serde(default)]
+    max_duration: f64,
+}
+
 //https://github.com/serde-rs/serde/pull/238
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all="snake_case")]
 struct Plan {
     #[serde(default)]
     actual_cost: f64,
@@ -117,26 +138,65 @@ impl fmt::Display for Plan {
     }
 }
 
-fn calculate_planner_estimate(mut plan: Plan) -> Plan {
-    plan.planner_row_estimate_factor = 0.0;
+impl Plan {
+    fn calculate_planner_estimate(&mut self) {
+        self.planner_row_estimate_factor = 0.0;
 
-    if plan.plan_rows == plan.actual_rows {
-        return plan;
-    }
+        if self.plan_rows == self.actual_rows {
+            return;
+        }
 
-    plan.planner_row_estimate_direction = UNDER.to_string();
-    if plan.plan_rows != 0 {
-        plan.planner_row_estimate_factor = plan.actual_rows as f64 / plan.plan_rows as f64;
-    }
+        self.planner_row_estimate_direction = UNDER.to_string();
+        if self.plan_rows != 0 {
+            self.planner_row_estimate_factor = self.actual_rows as f64 / self.plan_rows as f64;
+        }
 
-    if plan.planner_row_estimate_factor < 1.0 {
-        plan.planner_row_estimate_factor = 0.0;
-        plan.planner_row_estimate_direction = OVER.to_string();
-        if plan.actual_rows != 0 {
-            plan.planner_row_estimate_factor = plan.plan_rows as f64 / plan.actual_rows as f64;
+        if self.planner_row_estimate_factor < 1.0 {
+            self.planner_row_estimate_factor = 0.0;
+            self.planner_row_estimate_direction = OVER.to_string();
+            if self.actual_rows != 0 {
+                self.planner_row_estimate_factor = self.plan_rows as f64 / self.actual_rows as f64;
+            }
         }
     }
-    return plan;
+}
+
+impl Explain {
+    pub fn process_explain(&mut self) {
+        self.plan.calculate_planner_estimate();
+        self.calculate_actuals();
+        self.calculate_maximums();
+    }
+    pub fn calculate_actuals(&mut self) {
+        self.plan.actual_duration = self.plan.actual_total_time;
+        self.plan.actual_cost = self.plan.total_cost;
+
+        for child in self.plan.plans.iter() {
+            if child.node_type != CTE_SCAN {
+                self.plan.actual_duration = self.plan.actual_duration - child.actual_total_time;
+                self.plan.actual_cost = self.plan.actual_cost - child.total_cost;
+            }
+        }
+
+        if self.plan.actual_cost < 0.0 {
+            self.plan.actual_cost = 0.0;
+        }
+
+        self.total_cost = self.total_cost + self.plan.actual_cost;
+
+        self.plan.actual_duration = self.plan.actual_duration * self.plan.actual_loops as f64;
+    }
+    pub fn calculate_maximums(&mut self) {
+        if self.max_rows < self.plan.actual_rows {
+            self.max_rows = self.plan.actual_rows
+        }
+        if self.max_cost < self.plan.actual_cost {
+            self.max_cost = self.plan.actual_cost
+        }
+        if self.max_duration < self.plan.actual_duration {
+            self.max_duration = self.plan.actual_duration
+        }
+    }
 }
 
 // a little smoke test...
@@ -148,9 +208,9 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let input: &str = &args[1];
     println!("args {}", input);
-    let plans: Vec<Plan> = serde_json::from_str(input).unwrap();
-    for plan in plans.iter() {
-        println!("plan {:#?}", plan)
+    let explains: Vec<Explain> = serde_json::from_str(input).unwrap();
+    for explain in explains.iter() {
+        println!("explain {:#?}", explain)
     }
     write_explain_stub()
 }
