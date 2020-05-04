c8de5d983254b7522760156f7ccba93e9ad3db08
Bruce P <treble37@users.noreply.github.com>
2020-05-04 15:57:12 -0700

    separated out libs

--- tests/test.rs
+++ tests/test.rs
@@ -1,3 +1,12 @@
 #[cfg(test)]
 mod tests {
+    use rustcmdpev::*;
+    #[test]
+    fn basic_process_explain_works() {
+        let str_input = "[[{\"Plan\":{\"Alias\":\"c0\",\"Node Type\":\"Seq Scan\",\"Parallel Aware\":false,\"Plan Rows\":50,\"Plan Width\":1572,\"Relation Name\":\"coaches\",\"Startup Cost\":0.0,\"Total Cost\":10.5}}]]";
+        let explains: Vec<Explain> = serde_json::from_str(str_input).unwrap();
+        for explain in explains.iter() {
+            println!("explain {:#?}", explain)
+        }
+    }
 }
