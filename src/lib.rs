76729824a1e72d05ff532b9753daf6d438755925
Bruce P <treble37@users.noreply.github.com>
2020-06-12 17:13:44 -0700

    Add write_plan function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use serde::{Deserialize, Serialize};
 use colored::*;
+use phf::phf_map;
+use serde::{Deserialize, Serialize};
 use std::fmt;
 
 const UNDER: &str = "Under";
@@ -8,6 +9,24 @@ const CTE_SCAN: &str = "CTE Scan";
 
 type NodeType = String;
 type EstimateDirection = String;
+
+static DESCRIPTIONS: phf::Map<&'static str, &'static str> = phf_map! {
+    "Append" =>          "Used in a UNION to merge multiple record sets by appending them together.",
+    "Limit" =>           "Returns a specified number of rows from a record set.",
+    "Sort" =>            "Sorts a record set based on the specified sort key.",
+    "Nested Loop" =>      "Merges two record sets by looping through every record in the first set and trying to find a match in the second set. All matching records are returned.",
+    "Merge Join" =>       "Merges two record sets by first sorting them on a join key.",
+    "Hash" =>            "Generates a hash table from the records in the input recordset. Hash is used by Hash Join.",
+    "Hash Join" =>        "Joins to record sets by hashing one of them (using a Hash Scan).",
+    "Aggregate" =>       "Groups records together based on a GROUP BY or aggregate function (e.g. sum()).",
+    "Hashaggregate" =>   "Groups records together based on a GROUP BY or aggregate function (e.g. sum()). Hash Aggregate uses a hash to first organize the records by a key.",
+    "Seq Scan" =>    "Finds relevant records by sequentially scanning the input record set. When reading from a table, Seq Scans (unlike Index Scans) perform a single read operation (only the table is read).",
+    "Index Scan" =>       "Finds relevant records based on an Index. Index Scans perform 2 read operations: one to read the index and another to read the actual value from the table.",
+    "Index Only Scan" =>   "Finds relevant records based on an Index. Index Only Scans perform a single read operation from the index and do not read from the corresponding table.",
+    "Bitmap Heap Scan" =>  "Searches through the pages returned by the Bitmap Index Scan for relevant rows.",
+    "Bitmap Index Scan" => "Uses a Bitmap Index (index which uses 1 bit per page) to find all relevant pages. Results of this node are fed to the Bitmap Heap Scan.",
+    "CTE Scan" =>         "Performs a sequential scan of Common Table Expression (CTE) query results. Note that results of a CTE are materialized (calculated and temporarily stored).",
+};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Explain {
@@ -53,11 +72,11 @@ pub struct Plan {
     pub filter: String,
     #[serde(default, rename(deserialize = "Group Key"))]
     pub group_key: Vec<String>,
-    #[serde(default, rename(deserialize = "Hash Condition"))]
+    #[serde(default, rename(deserialize = "Hash Cond"))]
     pub hash_condition: String,
     #[serde(default, rename(deserialize = "Heap Fetches"))]
     pub heap_fetches: u64,
-    #[serde(default, rename(deserialize = "Index Condition"))]
+    #[serde(default, rename(deserialize = "Index Cond"))]
     pub index_condition: String,
     #[serde(default, rename(deserialize = "Index Name"))]
     pub index_name: String,
@@ -287,7 +306,6 @@ fn process_explain(explain: Explain) -> Explain {
 }
 
 fn process_child_plans(explain: Explain, plans: Vec<Plan>) -> (Explain, Vec<Plan>) {
-    //need to figure out how to deal with recursive process_plan
     let mut new_explain: Explain = explain;
     let mut new_plans: Vec<Plan> = plans;
     for mut child_plan in new_plans.iter_mut() {
@@ -317,25 +335,262 @@ pub fn process_all(explain: Explain) -> Explain {
 }
 
 fn duration_to_string(value: f64) -> colored::ColoredString {
-	if value < 100.0 {
-		return format!("{0:.2} ms", value).green()
-	} else if value < 1000.0 {
-		return format!("{0:.2} ms", value).yellow()
-	} else if value < 60000.0 {
-		return format!("{0:.2} s", value/2000.0).red()
-	} else {
-		return format!("{0:.2} m", value/60000.0).red()
-	}
+    if value < 100.0 {
+        return format!("{0:.2} ms", value).green();
+    } else if value < 1000.0 {
+        return format!("{0:.2} ms", value).yellow();
+    } else if value < 60000.0 {
+        return format!("{0:.2} s", value / 2000.0).red();
+    } else {
+        return format!("{0:.2} m", value / 60000.0).red();
+    }
 }
 
-pub fn write_explain(explain: Explain, width: i32) {
+pub fn write_explain(explain: Explain, width: usize) {
     println!("○ Total Cost {}", explain.total_cost);
-    println!("○ Planning Time: {}", duration_to_string(explain.planning_time));
-    println!("○ Execution Time: {}", duration_to_string(explain.execution_time));
-    println!("{}\n", "┬".cyan());
+    println!(
+        "○ Planning Time: {}",
+        duration_to_string(explain.planning_time)
+    );
+    println!(
+        "○ Execution Time: {}",
+        duration_to_string(explain.execution_time)
+    );
+    println!("{}", color_format("┬".to_string(), "output"));
+    let p: Plan = explain.clone().plan;
+    write_plan(
+        explain.clone(),
+        &p,
+        "".to_string(),
+        0,
+        width,
+        explain.plan.plans.len() == 1,
+    )
 }
 
-pub fn visualize(input: String, width: i32) -> Explain {
+pub fn color_format(s: String, format: &str) -> colored::ColoredString {
+    match format {
+        "prefix" => s.bright_black(),
+        "muted" => s.bright_black(),
+        "bold" => s.bright_white(),
+        "good" => s.green(),
+        "warning" => s.yellow(),
+        "critical" => s.red(),
+        "output" => s.cyan(),
+        _ => s.green(),
+    }
+}
+
+fn format_details(plan: Plan) -> String {
+    let mut details = vec![];
+
+    if plan.scan_direction != "" {
+        details.push(plan.scan_direction);
+    }
+
+    if plan.strategy != "" {
+        details.push(plan.strategy);
+    }
+
+    if details.len() > 0 {
+        details.join(", ");
+    }
+
+    return "".to_string();
+}
+
+fn format_tags(plan: Plan) -> String {
+    let mut tags = vec![];
+
+    if plan.slowest {
+        tags.push(" slowest ");
+    }
+    if plan.costliest {
+        tags.push(" costliest ");
+    }
+    if plan.largest {
+        tags.push(" largest ");
+    }
+    if plan.planner_row_estimate_factor >= 100.0 {
+        tags.push(" bad estimate ");
+    }
+
+    return tags.join(" ");
+}
+
+fn get_terminator(index: usize, plan: Plan) -> String {
+    if index == 0 {
+        if plan.plans.len() == 0 {
+            return "⌡► ".to_string();
+        } else {
+            return "├►  ".to_string();
+        }
+    } else {
+        if plan.plans.len() == 0 {
+            return "   ".to_string();
+        } else {
+            return "│  ".to_string();
+        }
+    }
+}
+pub fn write_plan(
+    explain: Explain,
+    plan: &Plan,
+    prefix: String,
+    depth: i32,
+    width: usize,
+    last_child: bool,
+) {
+    let mut source_prefix: String = String::from(prefix.clone());
+    let mut current_prefix: String = prefix.clone();
+    println!("{}{}", color_format(source_prefix.clone(), "prefix"),
+             color_format("│".to_string(), "prefix"));
+    let mut joint: String = String::from("├");
+    if plan.plans.len() > 1 || last_child {
+        joint = "└".to_string();
+    }
+    println!(
+        "{}{} {}{} {}",
+        color_format(current_prefix.clone(), "prefix"),
+        color_format(joint + "─⌠", "prefix"),
+        color_format(plan.clone().node_type, "bold"),
+        format_details(plan.clone()),
+        format_tags(plan.clone())
+    );
+
+    if plan.plans.len() > 1 || last_child {
+        source_prefix = source_prefix + "  "
+    } else {
+        source_prefix = source_prefix + "│ "
+    }
+    current_prefix = source_prefix.clone() + "│ ";
+    let cols: usize = width - current_prefix.len();
+    let lines: Vec<String> = vec![format!(
+        "{}",
+        textwrap::fill(DESCRIPTIONS[plan.node_type.as_str()], cols)
+    )
+    .split("\n")
+    .collect()];
+    for line in lines {
+        println!(
+            "{}{}",
+            color_format(current_prefix.clone(), "prefix"),
+            color_format(line, "muted")
+        )
+    }
+    println!(
+        "{}○ Duration: {} {}",
+        color_format(current_prefix.clone(), "prefix"),
+        duration_to_string(plan.actual_duration),
+        (plan.actual_duration / explain.execution_time) * 100.0
+    );
+    println!(
+        "{}○ Cost: {} {}",
+        color_format(current_prefix.clone(), "prefix"),
+        plan.actual_cost,
+        (plan.actual_cost / explain.total_cost) * 100.0
+    );
+    println!(
+        "{}○ Rows: {}",
+        color_format(current_prefix.clone(), "prefix"),
+        plan.actual_rows
+    );
+    current_prefix = current_prefix + "  ";
+
+    if plan.join_type != "" {
+        println!(
+            "{}{} {}",
+            color_format(current_prefix.clone(), "prefix"),
+            color_format("join".to_string(), "muted"),
+            color_format(plan.clone().join_type, "muted")
+        )
+    }
+    if plan.relation_name != "" {
+        println!(
+            "{}{} {} {}",
+            color_format(current_prefix.clone(), "prefix"),
+            color_format("on".to_string(), "muted"),
+            color_format(plan.clone().schema, "muted"),
+            color_format(plan.clone().relation_name, "muted")
+        )
+    }
+
+    if plan.index_name != "" {
+        println!(
+            "{}{} {}",
+            color_format(current_prefix.clone(), "prefix"),
+            color_format("using".to_string(), "muted"),
+            plan.index_name
+        )
+    }
+
+    if plan.index_condition != "" {
+        println!(
+            "{}{} {}",
+            color_format(current_prefix.clone(), "prefix"),
+            color_format("condition".to_string(), "muted"),
+            plan.index_condition
+        )
+    }
+
+    if plan.filter != "" {
+        //outputFn("%v %v %v", mutedFormat("filter"), plan.Filter, mutedFormat(fmt.Sprintf("[-%v rows]", humanize.Comma(int64(plan.RowsRemovedByFilter)))))
+        println!(
+            "{}{} {} [-{} rows]",
+            color_format(current_prefix.clone(), "prefix"),
+            color_format("filter".to_string(), "muted"),
+            plan.filter,
+            color_format(plan.rows_removed_by_filter.to_string(), "muted")
+        );
+    }
+    if plan.hash_condition != "" {
+        println!(
+            "{}{} {}",
+            color_format(current_prefix.clone(), "prefix"),
+            color_format("on".to_string(), "muted"),
+            plan.hash_condition
+        )
+    }
+    if plan.cte_name != "" {
+        println!(
+            "{}CTE {}",
+            color_format(current_prefix.clone(), "prefix"),
+            plan.cte_name
+        )
+    }
+
+    if plan.planner_row_estimate_factor != 0.0 {
+        //outputFn("%v %vestimated %v %.2fx", mutedFormat("rows"), plan.PlannerRowEstimateDirection, mutedFormat("by"), plan.PlannerRowEstimateFactor)
+        println!(
+            "{}{} {}estimated {} {}x",
+            color_format(current_prefix.clone(), "prefix"),
+            color_format("rows".to_string(), "muted"),
+            plan.planner_row_estimate_direction,
+            color_format("by".to_string(), "muted"),
+            plan.planner_row_estimate_factor
+        )
+    }
+
+    current_prefix = source_prefix.clone();
+
+    if plan.output.len() > 0 {
+        let joined_output: String = plan.output.join(" + ");
+        let wrapped_output: String = textwrap::fill(joined_output.as_str(), cols);
+        let split_output: Vec<&str> = wrapped_output.split("\n").collect();
+        for (index, line) in split_output.iter().enumerate() {
+            println!(
+                "{}{}",
+                color_format(current_prefix.clone(), "prefix"),
+                color_format(get_terminator(index, plan.clone()) + line, "prefix")
+            )
+        }
+    }
+    for (index, child_plan) in plan.plans.iter().enumerate() {
+        write_plan(explain.clone(), child_plan, source_prefix.clone(), depth+1, width, index == plan.plans.len()-1)
+    }
+}
+
+pub fn visualize(input: String, width: usize) -> Explain {
     let explains: Vec<Explain> = serde_json::from_str(input.as_str()).unwrap();
     let mut explain: Explain = explains.into_iter().nth(0).unwrap();
     explain = process_all(explain);
