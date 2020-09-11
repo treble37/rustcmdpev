4057d1749b4f798edbc63747bc797c7973eed3d9
Bruce P <treble37@users.noreply.github.com>
2020-09-11 16:30:28 -0700

    Update README and main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,5 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input: &str = &args[1];
-    println!("{}", input);
     rustcmdpev::visualize(input.to_string(), 60);
 }
