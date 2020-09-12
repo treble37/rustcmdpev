1250cfc19005ac1c93f546984fda059fcfd226be
Bruce P <treble37@users.noreply.github.com>
2020-09-11 20:41:33 -0700

    move Plan to its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 use colored::*;
 use phf::phf_map;
 use serde::{Deserialize, Serialize};
-use std::fmt;
+mod protocol;
 
 const UNDER: &str = "Under";
 const OVER: &str = "Over";
 const CTE_SCAN: &str = "CTE Scan";
 
-type NodeType = String;
-type EstimateDirection = String;
 
 static DESCRIPTIONS: phf::Map<&'static str, &'static str> = phf_map! {
     "Append" =>          "Used in a UNION to merge multiple record sets by appending them together.",
@@ -33,7 +31,7 @@ static DESCRIPTIONS: phf::Map<&'static str, &'static str> = phf_map! {
 pub struct Explain {
     //TODO: add Triggers back, add default for plan?
     #[serde(default, rename(deserialize = "Plan"))]
-    pub plan: Plan,
+    pub plan: protocol::Plan,
     #[serde(default, rename(deserialize = "Planning Time"))]
     pub planning_time: f64,
     #[serde(default, rename(deserialize = "Execution Time"))]
@@ -48,171 +46,11 @@ pub struct Explain {
     pub max_duration: f64,
 }
 
-//https://github.com/serde-rs/serde/pull/238
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Plan {
-    #[serde(default, rename(deserialize = "Actual Cost"))]
-    pub actual_cost: f64,
-    #[serde(default, rename(deserialize = "Actual Duration"))]
-    pub actual_duration: f64,
-    #[serde(default, rename(deserialize = "Actual Loops"))]
-    pub actual_loops: u64,
-    #[serde(default, rename(deserialize = "Actual Rows"))]
-    pub actual_rows: u64,
-    #[serde(default, rename(deserialize = "Actual Startup Time"))]
-    pub actual_startup_time: f64,
-    #[serde(default, rename(deserialize = "Actual Total Time"))]
-    pub actual_total_time: f64,
-    #[serde(default, rename(deserialize = "Alias"))]
-    pub alias: String,
-    #[serde(default, rename(deserialize = "Costliest"))]
-    pub costliest: bool,
-    #[serde(default, rename(deserialize = "CTE Name"))]
-    pub cte_name: String,
-    #[serde(default, rename(deserialize = "Filter"))]
-    pub filter: String,
-    #[serde(default, rename(deserialize = "Group Key"))]
-    pub group_key: Vec<String>,
-    #[serde(default, rename(deserialize = "Hash Cond"))]
-    pub hash_condition: String,
-    #[serde(default, rename(deserialize = "Heap Fetches"))]
-    pub heap_fetches: u64,
-    #[serde(default, rename(deserialize = "Index Cond"))]
-    pub index_condition: String,
-    #[serde(default, rename(deserialize = "Index Name"))]
-    pub index_name: String,
-    #[serde(default, rename(deserialize = "I/O Read Time"))]
-    pub io_read_time: f64,
-    #[serde(default, rename(deserialize = "I/O Write Time"))]
-    pub io_write_time: f64,
-    #[serde(default, rename(deserialize = "Join Type"))]
-    pub join_type: String,
-    #[serde(default)]
-    pub largest: bool,
-    #[serde(default, rename(deserialize = "Local Dirtied Blocks"))]
-    pub local_dirtied_blocks: u64,
-    #[serde(default, rename(deserialize = "Local Hit Blocks"))]
-    pub local_hit_blocks: u64,
-    #[serde(default, rename(deserialize = "Local Read Blocks"))]
-    pub local_read_blocks: u64,
-    #[serde(default, rename(deserialize = "Local Written Blocks"))]
-    pub local_written_blocks: u64,
-    #[serde(default, rename(deserialize = "Node Type"))]
-    pub node_type: NodeType,
-    #[serde(default, rename(deserialize = "Output"))]
-    pub output: Vec<String>,
-    #[serde(default, rename(deserialize = "Parent Relationship"))]
-    pub parent_relationship: String,
-    #[serde(default)]
-    pub planner_row_estimate_direction: EstimateDirection,
-    #[serde(default)]
-    pub planner_row_estimate_factor: f64,
-    #[serde(default, rename(deserialize = "Plan Rows"))]
-    pub plan_rows: u64,
-    #[serde(default, rename(deserialize = "Plan Width"))]
-    pub plan_width: u64,
-    #[serde(default, rename(deserialize = "Relation Name"))]
-    pub relation_name: String,
-    #[serde(default, rename(deserialize = "Rows Removed By Filter"))]
-    pub rows_removed_by_filter: u64,
-    #[serde(default, rename(deserialize = "Rows Removed By Index Recheck"))]
-    pub rows_removed_by_index_recheck: u64,
-    #[serde(default, rename(deserialize = "Scan Direction"))]
-    pub scan_direction: String,
-    #[serde(default, rename(deserialize = "Schema"))]
-    pub schema: String,
-    #[serde(default, rename(deserialize = "Shared Dirtied Blocks"))]
-    pub shared_dirtied_blocks: u64,
-    #[serde(default, rename(deserialize = "Shared Hit Blocks"))]
-    pub shared_hit_blocks: u64,
-    #[serde(default, rename(deserialize = "Shared Read Blocks"))]
-    pub shared_read_blocks: u64,
-    #[serde(default, rename(deserialize = "Shared Written Blocks"))]
-    pub shared_written_blocks: u64,
-    #[serde(default)]
-    pub slowest: bool,
-    #[serde(default, rename(deserialize = "Startup Cost"))]
-    pub startup_cost: f64,
-    #[serde(default, rename(deserialize = "Strategy"))]
-    pub strategy: String,
-    #[serde(default, rename(deserialize = "Temp Read Blocks"))]
-    pub temp_read_blocks: u64,
-    #[serde(default, rename(deserialize = "Temp Written Blocks"))]
-    pub temp_written_blocks: u64,
-    #[serde(default, rename(deserialize = "Total Cost"))]
-    pub total_cost: f64,
-    #[serde(default, rename(deserialize = "Plans"))]
-    pub plans: Vec<Plan>,
-}
-impl fmt::Display for Plan {
-    // This trait requires `fmt` with this exact signature.
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Write strictly the first element into the supplied output
-        // stream: `f`. Returns `fmt::Result` which indicates whether the
-        // operation succeeded or failed. Note that `write!` uses syntax which
-        // is very similar to `println!`.
-        //write!(f, "{}", self.0)
-        write!(f, "{}", self)
-    }
-}
-
-impl Default for Plan {
-    fn default() -> Plan {
-        Plan {
-            actual_cost: 0.0,
-            actual_duration: 0.0,
-            actual_loops: 0,
-            actual_rows: 0,
-            actual_startup_time: 0.0,
-            actual_total_time: 0.0,
-            alias: String::from(""),
-            costliest: false,
-            cte_name: String::from(""),
-            filter: String::from(""),
-            group_key: Vec::new(),
-            hash_condition: String::from(""),
-            heap_fetches: 0,
-            index_condition: String::from(""),
-            index_name: String::from(""),
-            io_read_time: 0.0,
-            io_write_time: 0.0,
-            join_type: String::from(""),
-            largest: false,
-            local_dirtied_blocks: 0,
-            local_hit_blocks: 0,
-            local_read_blocks: 0,
-            local_written_blocks: 0,
-            node_type: String::from(""),
-            output: Vec::new(),
-            parent_relationship: String::from(""),
-            planner_row_estimate_direction: String::from(""),
-            planner_row_estimate_factor: 0.0,
-            plan_rows: 0,
-            plan_width: 0,
-            relation_name: String::from(""),
-            rows_removed_by_filter: 0,
-            rows_removed_by_index_recheck: 0,
-            scan_direction: String::from(""),
-            schema: String::from(""),
-            shared_dirtied_blocks: 0,
-            shared_hit_blocks: 0,
-            shared_read_blocks: 0,
-            shared_written_blocks: 0,
-            slowest: false,
-            startup_cost: 0.0,
-            strategy: String::from(""),
-            temp_read_blocks: 0,
-            temp_written_blocks: 0,
-            total_cost: 0.0,
-            plans: Vec::new(),
-        }
-    }
-}
 
 impl Default for Explain {
     fn default() -> Explain {
         Explain {
-            plan: Plan {
+            plan: protocol::Plan {
                 ..Default::default()
             },
             planning_time: 0.0,
@@ -226,8 +64,8 @@ impl Default for Explain {
 }
 
 // https://www.forrestthewoods.com/blog/should-small-rust-structs-be-passed-by-copy-or-by-borrow/
-pub fn calculate_planner_estimate(plan: Plan) -> Plan {
-    let mut new_plan: Plan = plan;
+pub fn calculate_planner_estimate(plan: protocol::Plan) -> protocol::Plan {
+    let mut new_plan: protocol::Plan = plan;
     new_plan.planner_row_estimate_factor = 0.0;
 
     if new_plan.plan_rows == new_plan.actual_rows {
@@ -251,8 +89,8 @@ pub fn calculate_planner_estimate(plan: Plan) -> Plan {
     new_plan
 }
 
-pub fn calculate_actuals(explain: Explain, plan: Plan) -> (Explain, Plan) {
-    let mut new_plan: Plan = plan;
+pub fn calculate_actuals(explain: Explain, plan: protocol::Plan) -> (Explain, protocol::Plan) {
+    let mut new_plan: protocol::Plan = plan;
     let mut new_explain: Explain = explain;
     new_plan.actual_duration = new_plan.actual_total_time;
     new_plan.actual_cost = new_plan.total_cost;
@@ -271,7 +109,7 @@ pub fn calculate_actuals(explain: Explain, plan: Plan) -> (Explain, Plan) {
     (new_explain, new_plan)
 }
 
-pub fn calculate_maximums(explain: Explain, plan: Plan) -> Explain {
+pub fn calculate_maximums(explain: Explain, plan: protocol::Plan) -> Explain {
     let mut new_explain: Explain = explain;
     if new_explain.max_rows < plan.actual_rows {
         new_explain.max_rows = plan.actual_rows
@@ -285,8 +123,8 @@ pub fn calculate_maximums(explain: Explain, plan: Plan) -> Explain {
     new_explain
 }
 
-pub fn calculate_outlier_nodes(explain: Explain, plan: Plan) -> Plan {
-    let mut new_plan: Plan = plan.clone();
+pub fn calculate_outlier_nodes(explain: Explain, plan: protocol::Plan) -> protocol::Plan {
+    let mut new_plan: protocol::Plan = plan.clone();
     new_plan.costliest = new_plan.actual_cost == explain.max_cost;
     new_plan.largest = new_plan.actual_rows == explain.max_rows;
     new_plan.slowest = new_plan.actual_duration == explain.max_duration;
@@ -306,9 +144,9 @@ fn process_explain(explain: Explain) -> Explain {
     new_explain
 }
 
-fn process_child_plans(explain: Explain, plans: Vec<Plan>) -> (Explain, Vec<Plan>) {
+fn process_child_plans(explain: Explain, plans: Vec<protocol::Plan>) -> (Explain, Vec<protocol::Plan>) {
     let mut new_explain: Explain = explain;
-    let mut new_plans: Vec<Plan> = plans;
+    let mut new_plans: Vec<protocol::Plan> = plans;
     for mut child_plan in new_plans.iter_mut() {
         *child_plan = calculate_planner_estimate(child_plan.clone());
         let (e, p) = calculate_actuals(new_explain.clone(), child_plan.clone());
@@ -332,7 +170,7 @@ pub fn process_all(explain: Explain) -> Explain {
         new_explain = e;
         new_explain.plan.plans = ps;
     }
-    let outlier_plan: Plan = calculate_outlier_nodes(new_explain.clone(), new_explain.clone().plan);
+    let outlier_plan: protocol::Plan = calculate_outlier_nodes(new_explain.clone(), new_explain.clone().plan);
     new_explain.plan = outlier_plan;
     new_explain
 }
@@ -360,7 +198,7 @@ pub fn write_explain(explain: Explain, width: usize) {
         duration_to_string(explain.execution_time)
     );
     println!("{}", color_format("┬".to_string(), "output"));
-    let p: Plan = explain.clone().plan;
+    let p: protocol::Plan = explain.clone().plan;
     write_plan(
         explain.clone(),
         &p,
@@ -385,7 +223,7 @@ pub fn color_format(s: String, format: &str) -> colored::ColoredString {
     }
 }
 
-fn format_details(plan: Plan) -> String {
+fn format_details(plan: protocol::Plan) -> String {
     let mut details = vec![];
 
     if plan.scan_direction != "" {
@@ -403,7 +241,7 @@ fn format_details(plan: Plan) -> String {
     return "".to_string();
 }
 
-fn format_tags(plan: Plan) -> String {
+fn format_tags(plan: protocol::Plan) -> String {
     let mut tags = vec![];
 
     if plan.slowest {
@@ -421,7 +259,7 @@ fn format_tags(plan: Plan) -> String {
     return tags.join(" ");
 }
 
-fn get_terminator(index: usize, plan: Plan) -> String {
+fn get_terminator(index: usize, plan: protocol::Plan) -> String {
     if index == 0 {
         if plan.plans.len() == 0 {
             return "⌡► ".to_string();
@@ -442,7 +280,7 @@ pub fn format_percent(number: f64, precision: usize) -> String {
 
 pub fn write_plan(
     explain: Explain,
-    plan: &Plan,
+    plan: &protocol::Plan,
     prefix: String,
     depth: i32,
     width: usize,
---
+++ src/protocol/plan.rs
@@ -0,0 +1,166 @@
+//https://github.com/serde-rs/serde/pull/238
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+type EstimateDirection = String;
+type NodeType = String;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Plan {
+    #[serde(default, rename(deserialize = "Actual Cost"))]
+    pub actual_cost: f64,
+    #[serde(default, rename(deserialize = "Actual Duration"))]
+    pub actual_duration: f64,
+    #[serde(default, rename(deserialize = "Actual Loops"))]
+    pub actual_loops: u64,
+    #[serde(default, rename(deserialize = "Actual Rows"))]
+    pub actual_rows: u64,
+    #[serde(default, rename(deserialize = "Actual Startup Time"))]
+    pub actual_startup_time: f64,
+    #[serde(default, rename(deserialize = "Actual Total Time"))]
+    pub actual_total_time: f64,
+    #[serde(default, rename(deserialize = "Alias"))]
+    pub alias: String,
+    #[serde(default, rename(deserialize = "Costliest"))]
+    pub costliest: bool,
+    #[serde(default, rename(deserialize = "CTE Name"))]
+    pub cte_name: String,
+    #[serde(default, rename(deserialize = "Filter"))]
+    pub filter: String,
+    #[serde(default, rename(deserialize = "Group Key"))]
+    pub group_key: Vec<String>,
+    #[serde(default, rename(deserialize = "Hash Cond"))]
+    pub hash_condition: String,
+    #[serde(default, rename(deserialize = "Heap Fetches"))]
+    pub heap_fetches: u64,
+    #[serde(default, rename(deserialize = "Index Cond"))]
+    pub index_condition: String,
+    #[serde(default, rename(deserialize = "Index Name"))]
+    pub index_name: String,
+    #[serde(default, rename(deserialize = "I/O Read Time"))]
+    pub io_read_time: f64,
+    #[serde(default, rename(deserialize = "I/O Write Time"))]
+    pub io_write_time: f64,
+    #[serde(default, rename(deserialize = "Join Type"))]
+    pub join_type: String,
+    #[serde(default)]
+    pub largest: bool,
+    #[serde(default, rename(deserialize = "Local Dirtied Blocks"))]
+    pub local_dirtied_blocks: u64,
+    #[serde(default, rename(deserialize = "Local Hit Blocks"))]
+    pub local_hit_blocks: u64,
+    #[serde(default, rename(deserialize = "Local Read Blocks"))]
+    pub local_read_blocks: u64,
+    #[serde(default, rename(deserialize = "Local Written Blocks"))]
+    pub local_written_blocks: u64,
+    #[serde(default, rename(deserialize = "Node Type"))]
+    pub node_type: NodeType,
+    #[serde(default, rename(deserialize = "Output"))]
+    pub output: Vec<String>,
+    #[serde(default, rename(deserialize = "Parent Relationship"))]
+    pub parent_relationship: String,
+    #[serde(default)]
+    pub planner_row_estimate_direction: EstimateDirection,
+    #[serde(default)]
+    pub planner_row_estimate_factor: f64,
+    #[serde(default, rename(deserialize = "Plan Rows"))]
+    pub plan_rows: u64,
+    #[serde(default, rename(deserialize = "Plan Width"))]
+    pub plan_width: u64,
+    #[serde(default, rename(deserialize = "Relation Name"))]
+    pub relation_name: String,
+    #[serde(default, rename(deserialize = "Rows Removed By Filter"))]
+    pub rows_removed_by_filter: u64,
+    #[serde(default, rename(deserialize = "Rows Removed By Index Recheck"))]
+    pub rows_removed_by_index_recheck: u64,
+    #[serde(default, rename(deserialize = "Scan Direction"))]
+    pub scan_direction: String,
+    #[serde(default, rename(deserialize = "Schema"))]
+    pub schema: String,
+    #[serde(default, rename(deserialize = "Shared Dirtied Blocks"))]
+    pub shared_dirtied_blocks: u64,
+    #[serde(default, rename(deserialize = "Shared Hit Blocks"))]
+    pub shared_hit_blocks: u64,
+    #[serde(default, rename(deserialize = "Shared Read Blocks"))]
+    pub shared_read_blocks: u64,
+    #[serde(default, rename(deserialize = "Shared Written Blocks"))]
+    pub shared_written_blocks: u64,
+    #[serde(default)]
+    pub slowest: bool,
+    #[serde(default, rename(deserialize = "Startup Cost"))]
+    pub startup_cost: f64,
+    #[serde(default, rename(deserialize = "Strategy"))]
+    pub strategy: String,
+    #[serde(default, rename(deserialize = "Temp Read Blocks"))]
+    pub temp_read_blocks: u64,
+    #[serde(default, rename(deserialize = "Temp Written Blocks"))]
+    pub temp_written_blocks: u64,
+    #[serde(default, rename(deserialize = "Total Cost"))]
+    pub total_cost: f64,
+    #[serde(default, rename(deserialize = "Plans"))]
+    pub plans: Vec<Plan>,
+}
+impl fmt::Display for Plan {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        //write!(f, "{}", self.0)
+        write!(f, "{}", self)
+    }
+}
+
+impl Default for Plan {
+    fn default() -> Plan {
+        Plan {
+            actual_cost: 0.0,
+            actual_duration: 0.0,
+            actual_loops: 0,
+            actual_rows: 0,
+            actual_startup_time: 0.0,
+            actual_total_time: 0.0,
+            alias: String::from(""),
+            costliest: false,
+            cte_name: String::from(""),
+            filter: String::from(""),
+            group_key: Vec::new(),
+            hash_condition: String::from(""),
+            heap_fetches: 0,
+            index_condition: String::from(""),
+            index_name: String::from(""),
+            io_read_time: 0.0,
+            io_write_time: 0.0,
+            join_type: String::from(""),
+            largest: false,
+            local_dirtied_blocks: 0,
+            local_hit_blocks: 0,
+            local_read_blocks: 0,
+            local_written_blocks: 0,
+            node_type: String::from(""),
+            output: Vec::new(),
+            parent_relationship: String::from(""),
+            planner_row_estimate_direction: String::from(""),
+            planner_row_estimate_factor: 0.0,
+            plan_rows: 0,
+            plan_width: 0,
+            relation_name: String::from(""),
+            rows_removed_by_filter: 0,
+            rows_removed_by_index_recheck: 0,
+            scan_direction: String::from(""),
+            schema: String::from(""),
+            shared_dirtied_blocks: 0,
+            shared_hit_blocks: 0,
+            shared_read_blocks: 0,
+            shared_written_blocks: 0,
+            slowest: false,
+            startup_cost: 0.0,
+            strategy: String::from(""),
+            temp_read_blocks: 0,
+            temp_written_blocks: 0,
+            total_cost: 0.0,
+            plans: Vec::new(),
+        }
+    }
+}
