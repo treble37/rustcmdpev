8aee91bea51dd12f8d567ad5aa08eefd65051e49
Bruce P <treble37@users.noreply.github.com>
2020-09-18 14:41:32 -0700

    Allow nested json to be passed as command line input arg

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use std::env;
+use std::io::{self, BufRead};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let input: &str = &args[1];
-    rustcmdpev::visualize(input.to_string(), 60);
+    let stdin = io::stdin();
+    let mut input: Vec<String> = vec![];
+    for line in stdin.lock().lines() {
+        let line = line.expect("Could not read line from standard in");
+        input.push(line.clone());
+    }
+    let joined_input: String = input[0..].join("\n");
+    rustcmdpev::visualize(joined_input, 60);
 }
